f007252b28d2302c75c4d24c2bb8ed715d9928a8
yuceL <ycL@DESKTOP-PG4QN3E>
2021-05-02 01:17:31 +0300

    interfaces

---
+++ interfaces/src/interfaces/Main.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Logger[] loggers = {new SmsLogger(), new EmailLogger()};
+		CustomerManager customerManager = new CustomerManager(loggers);
+		
+		Customer yucel = new Customer(1,"Yucel","Taskiran");
+		
+		customerManager.add(yucel);
+
+	}
+
+}
